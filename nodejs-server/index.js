ae70ca62b42cf88a891e0e58bb26b2f7f9363400
dvillarama <david.villarama@science37.com>
2019-08-16 17:23:22 +0000

    Generated code by SwaggerHub

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
-var serverPort = 11116;
+var serverPort = 8080;
 
 // swaggerRouter configuration
 var options = {
